04939da8f2b70fe5d1121098add2c78cf613b098
Arthur LE MOIGNE <arthur.lemoigne@gmail.com>
2021-04-06 18:01:32 +0200

    Add unit tests

--- quickfix-messages/src/lib.rs
+++ quickfix-messages/src/lib.rs
@@ -48,3 +48,77 @@ pub trait FromFixMessage: FixID {
     where
         Self: Sized;
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[derive(Debug, PartialEq)]
+    struct TestStruct {
+        value: String,
+    }
+
+    impl FixID for TestStruct {
+        const FIELD_ID: usize = 42;
+    }
+
+    impl AsFixMessage for TestStruct {
+        fn as_fix_value(&self) -> String {
+            self.value.clone()
+        }
+    }
+
+    impl FromFixMessage for TestStruct {
+        fn from_fix_value(value: &str) -> Result<Self, FixParseError> {
+            Ok(Self {
+                value: value.into(),
+            })
+        }
+    }
+
+    #[test]
+    fn test_struct_encode() {
+        let field = TestStruct {
+            value: "foobar".into(),
+        };
+        assert_eq!(field.encode_field(), "42=foobar".to_string());
+    }
+
+    #[derive(Debug, PartialEq)]
+    enum TestEnum {
+        Opt1,
+        Opt2,
+    }
+
+    impl FixID for TestEnum {
+        const FIELD_ID: usize = 29;
+    }
+
+    impl AsFixMessage for TestEnum {
+        fn as_fix_value(&self) -> String {
+            match *self {
+                Self::Opt1 => "opt1",
+                Self::Opt2 => "opt2",
+            }
+            .to_string()
+        }
+    }
+
+    impl FromFixMessage for TestEnum {
+        fn from_fix_value(value: &str) -> Result<Self, FixParseError> {
+            match value {
+                "opt1" => Ok(Self::Opt1),
+                "opt2" => Ok(Self::Opt2),
+                _ => Err(FixParseError::InvalidData),
+            }
+        }
+    }
+
+    #[test]
+    fn test_enum_encode() {
+        let field = TestEnum::Opt1;
+        assert_eq!(field.encode_field(), "29=opt1".to_string());
+        let field = TestEnum::Opt2;
+        assert_eq!(field.encode_field(), "29=opt2".to_string());
+    }
+}
