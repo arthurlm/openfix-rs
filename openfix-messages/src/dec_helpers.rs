00347fb00d0a66d3ee4e01e9c621d43d542421a0
Arthur LE MOIGNE <arthur.lemoigne@gmail.com>
2021-04-12 00:11:55 +0200

    Avoid temporary vec to parse payload fields

--- openfix-messages/src/dec_helpers.rs
+++ openfix-messages/src/dec_helpers.rs
@@ -7,17 +7,15 @@ const SEP_CHAR: u8 = 0x01;
 pub fn split_message_items<'a>(data: &'a [u8]) -> FixFieldItems<'a> {
     data.split(|x| *x == SEP_CHAR)
         .filter_map(|field| {
-            let fields: Vec<_> = field.splitn(2, |x| *x == '=' as u8).collect();
+            let mut fields = field.splitn(2, |x| *x == '=' as u8);
 
-            match fields[..] {
-                [field_id, field_data] => {
-                    let field_id = std::str::from_utf8(field_id).ok()?;
-                    let field_id = field_id.parse::<u32>().ok()?;
+            let field_id = fields.next()?;
+            let field_id = std::str::from_utf8(field_id).ok()?;
+            let field_id = field_id.parse::<u32>().ok()?;
 
-                    Some((field_id, field_data))
-                }
-                _ => None,
-            }
+            let field_data = fields.next()?;
+
+            Some((field_id, field_data))
         })
         .collect()
 }
